f710b6e94992d2fe06b4466e232057087a4fd02e
Pascal <pascal13yt@gmail.com>
2023-03-11 15:38:29 +0100

    L

--- Field.java
+++ Field.java
@@ -1,5 +1,17 @@
 import pokemon.Pokemon;
 
 public class Field {
-    private Pokemon pokemon;
+    private Pokemon pokemon[];
+
+    private void printState() {
+        System.out.println("You look around and see: ");
+        for (pokemon in pokemon) {
+            System.out.println(pokemon.name);
+        }
+    };
+
+    @Staticmethod
+    public void generateField() {
+    return Field
+    }
 }
